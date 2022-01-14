590c1cab81a45c03c65d561d0d4bf61a340fd6c7
Dan Burnley <dan@danops.uk>
2022-01-14 16:09:24 +0000

    Add user choice component to application

--- designer/client/index.tsx
+++ designer/client/index.tsx
@@ -6,6 +6,7 @@ import { initI18n } from "./i18n";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Designer from "./designer";
 import { SaveError } from "./pages/ErrorPages";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 initI18n();
 
@@ -13,30 +14,57 @@ function NoMatch() {
   return <div>404 Not found</div>;
 }
 
+function UserChoice() {
+  let [userState, updateUserState] = React.useState("user");
+  let [_, setcookie] = useCookies(["user"]);
+
+  let updateUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setcookie("user", userState, {
+      path: "/",
+      sameSite: "strict",
+    });
+  };
+
+  return (
+    <form onSubmit={(e) => updateUser(e)}>
+      <input
+        type="text"
+        onChange={(e) => updateUserState(e.target.value)}
+        value={userState}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
 export class App extends React.Component {
   render() {
     return (
       <Router basename="/app">
-        <div id="app">
-          <Switch>
-            <Route path="/designer/:id" component={Designer} />
-            <Route path="/" exact>
-              <LandingChoice />
-            </Route>
-            <Route path="/new" exact>
-              <NewConfig />
-            </Route>
-            <Route path="/choose-existing" exact>
-              <ChooseExisting />
-            </Route>
-            <Route path="/save-error" exact>
-              <SaveError />
-            </Route>
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </div>
+        <CookiesProvider>
+          <div id="app">
+            <UserChoice />
+            <Switch>
+              <Route path="/designer/:id" component={Designer} />
+              <Route path="/" exact>
+                <LandingChoice />
+              </Route>
+              <Route path="/new" exact>
+                <NewConfig />
+              </Route>
+              <Route path="/choose-existing" exact>
+                <ChooseExisting />
+              </Route>
+              <Route path="/save-error" exact>
+                <SaveError />
+              </Route>
+              <Route path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </div>
+        </CookiesProvider>
       </Router>
     );
   }
